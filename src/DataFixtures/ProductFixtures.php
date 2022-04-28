6ff6d2062b4c60aa7ae3730d7f209de4f3316f68
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-04-28 10:42:09 +0200

    Base of the project created and ready to go.

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,17 +2,25 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
+use App\Entity\Products;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $product = new Product();
-        $product->setName('pomme');
-        $product->set
+        $product = (new Products())
+        ->setName("pomme")
+        ->setBio(true)
+        ->setStock(10)
+        ->setPrice(200)
+        ->setDescription("Fruit issu d'un pommier")
+        ->setImg("http://SaisonCulture/assets/img/uneImage.jpg")
+        ->setActive(true)
+        ->setCreatedAt(new \DateTimeImmutable())
+        ->setModifiedAt(new \DateTimeImmutable());
+        $manager->persist($product);
 
         $manager->flush();
     }
