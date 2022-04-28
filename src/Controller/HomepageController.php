f96a4eff79892b990fcf4b91b0c73e80c442835b
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-04-28 12:11:42 +0200

    new controller added

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        return $this->render('homepage/accueil.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+    #[Route('/homepage', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        return $this->render('homepage/accueil.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+    #[Route('/homepage', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        return $this->render('homepage/accueil.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+    #[Route('/homepage', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        return $this->render('homepage/accueil.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+    #[Route('/homepage', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        return $this->render('homepage/accueil.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}
