9a42b234bc6d06c75ea75bfdddfd50c65b1ed60a
Justine <98328323+JustineD22@users.noreply.github.com>
2022-05-05 09:04:37 +0200

    modification des routes pour l'admin Route pour les catégories, manque d'autres route qui seront ajoutés

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,33 +1,77 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\CategoryFormType;
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/gestionutilisateurs', name: 'gestionutilisateurs')]
-    public function gestionUtilisateurs(): Response
-    {
-        return $this->render('admin/utilisateur.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
-    }
-    #[Route('/gestioncategories', name: 'gestioncategories')]
-    public function gestionCategories(): Response
+    #[Route('/category', name: 'app_category_index', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
     {
         return $this->render('admin/categories.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
-    }
-    #[Route('/gestionproduits', name: 'gestionproduits')]
-    public function gestionProduits(): Response
-    {
-        return $this->render('admin/produits.html.twig', [
-            'controller_name' => 'AdminController',
+            'categoryForm' => $categoryRepository->findAll(),
         ]);
     }
 
+    #[Route('/category/new', name: 'app_category_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category->setCreatedAt(new \DateTimeImmutable());
+            $categoryRepository->add($category);
+            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/newcategory.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/category/{id}', name: 'app_category_show', methods: ['GET'])]
+    public function show(Category $category): Response
+    {
+        return $this->render('admin/showcategory.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/admin/category/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Category $category, CategoryRepository $categoryRepository): Response
+    {
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categoryRepository->add($category);
+            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/editcategory.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('admin/category/{id}', name: 'app_category_delete', methods: ['POST'])]
+    public function delete(Request $request, Category $category, CategoryRepository $categoryRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $categoryRepository->remove($category);
+        }
+
+        return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
