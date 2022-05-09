9341cb9540e37558b0bbda88446d25ce4cafce3e
Justine <98328323+JustineD22@users.noreply.github.com>
2022-05-09 09:38:10 +0200

    admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 use App\Form\CategoryFormType;
 use App\Entity\Category;
+use App\Entity\User;
 use App\Repository\CategoryRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -81,4 +83,68 @@ class AdminController extends AbstractController
 
         return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/users', name: 'app_users_index', methods: ['GET'])]
+    public function userindex(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/utilisateur.html.twig', [
+            'UserForm' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/users/newusers', name: 'app_users_new', methods: ['GET', 'POST'])]
+    public function newuser(Request $request, UserRepository $userRepository): Response
+    {
+        $users = new User();
+
+        $form = $this->createForm(RegistrationFormType::class, $users);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $users->setCreatedAt(new \DateTimeImmutable());
+            $userRepository->add($users);
+            return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/newusers.html.twig', [
+            'users' => $users,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/users/{id}', name: 'app_users_show', methods: ['GET'])]
+    public function showusers(User $users): Response
+    {
+        return $this->render('admin/showusers.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/admin/users/{id}/edit', name: 'app_users_edit', methods: ['GET', 'POST'])]
+    public function editusers(Request $request, User $users, UserRepository $usersRepository): Response
+    {
+        $form = $this->createForm(RepositoryFormType::class, $users);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $usersRepository->add($users);
+            return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/editusers.html.twig', [
+            'users' => $users,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('admin/users/{id}', name: 'app_users_delete', methods: ['POST'])]
+    public function deleteusers(Request $request, User $users, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$users->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($users);
+        }
+
+        return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
+    }
+
 }
