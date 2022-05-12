5dea760fb946fad7d651b5cda5edba8a306b0d88
Justine <98328323+JustineD22@users.noreply.github.com>
2022-05-12 14:28:38 +0200

    formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,9 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -21,25 +19,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('roles', ChoiceType::class, [
-                'required' => true,
-                'mapped' => false,
-                'multiple' => false,
-                'expanded' => true,
-                'choices' => [
-                    'user' => 'ROLE_USER',
-                    'producer' => 'ROLE_PRODUCER',
-                ],
-            ])
             ->add('email', EmailType::class)
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -47,7 +27,7 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
@@ -57,51 +37,25 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            // ->add('name', TextType::class, [
-            //     'attr' => [
-            //         'class' => 'producer'
-            //     ],
-                
-            // ])
-            // ->add('company', TextType::class, [
-            //     'attr' => [
-            //         'class' => 'producer'
-            //     ],
-            // ])
-            // ->add('address1', TextType::class, [
-            //     'attr' => [
-            //         'class' => 'producer'
-            //     ],
-            // ])
-            // ->add('address2', TextType::class, [
-            //     'attr' => [
-            //         'class' => 'producer'
-            //     ],
-            // ])
-            // ->add('city', TextType::class, [
-            //     'attr' => [
-            //         'class' => 'producer'
-            //     ],
-            // ])
-            // ->add('zipCode', IntegerType::class, [
-            //     'attr' => [
-            //         'class' => 'producer'
-            //     ],
-            //     'constraints' => [
-            //         new Length([
-            //             'min' => 5,
-            //             'minMessage' => 'You have to write 5 characters for the zip code',
-            //             'max' => 5,
-            //             'maxMessage' => 'You have to write 5 characters for the zip code',
-            //         ])
-            //     ]
-            // ])
-            // ->add('phone', IntegerType::class, [
-            //     'attr' => [
-            //         'class' => 'producer'
-            //     ],
-            // ])
-        ;
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Accepter les conditions d\'utilisation',
+                    ]),
+                ],
+            ])
+
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'mapped' => false,
+                'multiple' => false,
+                'expanded' => true,
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Producteur' => 'ROLE_PRODUCER',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
